104a979065d3931fa009fffce8f100aa1d755d12
r4vc31 <r4vc31@gmail.com>
2024-09-07 21:29:52 -0500

    Refactor & Improvements

--- app.js
+++ app.js
@@ -1,24 +1,54 @@
+const numbers = document.querySelectorAll(".key.number");
+const operators = document.querySelectorAll(".key.operator");
+const equal = document.querySelector(".key.equal");
+const eraser = document.querySelector(".key.eraser");
+const cleaner = document.querySelector(".key.cleaner");
+
 const previousResult = document.querySelector(".previous-result");
 const currentResult = document.querySelector(".current-result");
+
 let number1;
 let operator;
 let number2;
 let isCompleted = false;
 let result;
 
-document.querySelectorAll(".key.number").forEach((element) => {
+numbers.forEach((element) => {
   element.addEventListener(
     "click",
     () => (currentResult.innerHTML += element.innerHTML)
   );
 });
 
-document.querySelectorAll(".key.operator").forEach((element) => {
+operators.forEach((element) => {
   element.addEventListener("click", () => {
-    if (!currentResult.innerHTML) {
+    console.log("Operator Click", element.innerHTML);
+
+    if (!currentResult.innerHTML && !previousResult.innerHTML) {
       alert("First, insert a number");
       return;
+    } else {
+      if (!number1) {
+        number1 = parseInt(currentResult.innerHTML);
+      } else {
+        number2 = parseInt(currentResult.innerHTML);
+      }
     }
+
+    if (number1 && operator && number2) {
+      isCompleted = true;
+    }
+
+    if (isCompleted) {
+      previousResult.innerHTML += currentResult.innerHTML;
+      calculateResult();
+      if (!number1) {
+        number1 = parseInt(currentResult.innerHTML);
+      } else {
+        number2 = parseInt(currentResult.innerHTML);
+      }
+    }
+
     switch (element.innerHTML) {
       case "➕":
         operator = "+";
@@ -33,36 +63,31 @@ document.querySelectorAll(".key.operator").forEach((element) => {
         operator = "/";
         break;
     }
-    if (!operator) {
+
+    if (!operator && result===null) {
       alert("First, insert an operator");
       return;
     }
 
-    if(!number1){
-      number1 = parseInt(currentResult.innerHTML);
-    }else{
-      number2 = parseInt(currentResult.innerHTML); 
-    }
-
-    if (number1 && operator && number2) {
-      isCompleted = true;
-    }
-
-    if(!isCompleted){
+    if (!isCompleted && operator) {
       currentResult.innerHTML += operator;
-      if(result){
-        previousResult.innerHTML = '';
+      if (result!==null) {
+        previousResult.innerHTML = "";
       }
     }
-
-    previousResult.innerHTML += currentResult.innerHTML;
+    
+    if(currentResult.innerHTML != result){
+      previousResult.innerHTML += currentResult.innerHTML;
     currentResult.innerHTML = "";
+    }
+
+    console.log(number1, operator, number2);
+    
   });
 });
 
-const eraser = document.querySelector(".key.eraser");
 eraser.addEventListener("click", () => {
-  if (operator) {
+  if (operator && !currentResult.innerHTML) {
     currentResult.innerHTML = previousResult.innerHTML.slice(0, -1);
     previousResult.innerHTML = "";
   } else {
@@ -70,28 +95,34 @@ eraser.addEventListener("click", () => {
   }
 });
 
-document.querySelector(".key.cleaner").addEventListener("click", () => {
+cleaner.addEventListener("click", () => {
   previousResult.innerHTML = "";
   currentResult.innerHTML = "";
   resetInfo();
   result = null;
 });
 
-const equal = document.querySelector(".key.equal");
 equal.addEventListener("click", () => {
-  if(isCompleted) {
-    const operation = number1+operator+number2;
-    console.log(operation);
-    result = eval(operation);
+  if (isCompleted) {
+    calculateResult();
+  }else if(previousResult.innerHTML){
+    result = eval(previousResult.innerHTML);
     currentResult.innerHTML = result;
-    resetInfo();
   }
 });
-
 
 function resetInfo() {
   number1 = null;
   operator = null;
   number2 = null;
   isCompleted = false;
+}
+
+function calculateResult() {
+  const operation = number1 + operator + number2;
+  console.log(operation);
+  result = eval(operation);
+  currentResult.innerHTML = result;
+  resetInfo();
+  console.log(result);
 }
